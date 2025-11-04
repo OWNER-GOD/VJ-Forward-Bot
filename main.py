7dd3210f8d47c4a3b10d59d533444a63f6fee39a
OWNER-GOD <ownergod78@gmail.com>
2025-11-04 10:35:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+from plugins from verification
 import asyncio, logging
 from config import Config
 from pyrogram import Client as VJ, idle
@@ -73,3 +74,4 @@ if __name__ == "__main__":
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
